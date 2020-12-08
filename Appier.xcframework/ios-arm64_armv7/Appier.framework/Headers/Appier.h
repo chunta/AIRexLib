ad2f57bcb1f8743e9393ae01b24afa78a51359c4
chunta <hi.chuntache@gmail.com>
2020-12-08 16:06:04 +0800

    6.1.0

---
+++ Appier.xcframework/ios-arm64_armv7/Appier.framework/Headers/Appier.h
@@ -0,0 +1,29 @@
+//
+//  Appier.h
+//  Appier
+//
+//  Created by chunta on 2020/6/23.
+//  Copyright © 2020 chunta. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Appier.
+FOUNDATION_EXPORT double AppierVersionNumber;
+
+//! Project version string for Appier.
+FOUNDATION_EXPORT const unsigned char AppierVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like
+
+
+#import <Appier/QGSdk.h>
+#import <Appier/QGWKWebView.h>
+#import <Appier/QGInbox.h>
+#import <Appier/AIQP.h>
+#import <Appier/AIQP+ReactNative.h>
+#import <Appier/QGNotificationSdk.h>
+#import <Appier/QGNotificationService.h>
+#import <Appier/QGNotificationContentViewController.h>
+#import <Appier/Legacy.h>
+#import <Appier/AIDPage.h>
